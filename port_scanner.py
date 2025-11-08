c228d871becf6d950a740d14ffb5c6c0dc8fb5d7
zevlo <104938351+zevlo@users.noreply.github.com>
2025-11-08 15:28:00 -0500

    Refactor port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -3,6 +3,7 @@ from queue import Queue
 from socket import AF_INET, gethostbyname, socket, SOCK_STREAM
 import threading
 
+
 def tcp_test(port: int, target_ip: str) -> None:
     with socket(AF_INET, SOCK_STREAM) as sock:
         sock.settimeout(1)
@@ -10,11 +11,13 @@ def tcp_test(port: int, target_ip: str) -> None:
         if result == 0:
             print(f"Opened Port: {port}")
 
+
 def worker(target_ip: str, queue: Queue) -> None:
     while not queue.empty():
         port = queue.get()
         tcp_test(port, target_ip)
         queue.task_done()
+
 
 def main(host: str, start_port: int, end_port: int) -> None:
     target_ip = gethostbyname(host)
@@ -22,17 +25,24 @@ def main(host: str, start_port: int, end_port: int) -> None:
     for port in range(start_port, end_port + 1):
         queue.put(port)
     for _ in range(100):  # Adjust the number of threads if necessary
-        t = threading.Thread(target=worker, args=(target_ip, queue,))
+        t = threading.Thread(
+            target=worker,
+            args=(
+                target_ip,
+                queue,
+            ),
+        )
         t.daemon = True
         t.start()
     queue.join()
     print("Scanning completed.")
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='TCP Port Scanner')
-    parser.add_argument('host', help='Host to scan')
-    parser.add_argument('ports', help='Port range to scan, formatted as start-end')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="TCP Port Scanner")
+    parser.add_argument("host", help="Host to scan")
+    parser.add_argument("ports", help="Port range to scan, formatted as start-end")
     args = parser.parse_args()
 
-    start_port, end_port = map(int, args.ports.split('-'))
+    start_port, end_port = map(int, args.ports.split("-"))
     main(args.host, start_port, end_port)
